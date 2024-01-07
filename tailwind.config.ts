276220b46d1a0585579108f1c73d49d0866550df
ratidzo <ratygore@gmail.com>
2024-01-07 09:51:40 +0200

    setup: set tailwind colors from figma ui theme
    Remove boilerplate code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,29 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    /**
+     * Setup our color scheme from figma.
+     */
+
+    colors: {
+      grey: {
+        100: '#FAFAFA',
+        200: '#CBD4E6',
+        300: '#A1B0CC',
+        400: '#7C8DB0',
+        500: '#657491',
+        600: '#52556F',
+        700: '#52556F',
+        800: '36374A',
+        900: '#27273F'
+      }, 
+      purple: {
+        blue: '#605DEC',
+        light: '#BCBAF7'
       },
-    },
+      black: '#000000',
+      white: '#FFFFFF'
+    }
   },
   plugins: [],
 }

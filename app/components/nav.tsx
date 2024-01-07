10efd0302c6dcb918359f8c473128117c7a08ea1
ratidzo <ratygore@gmail.com>
2024-01-07 11:06:25 +0200

    dev: setup logo font and style

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,11 +1,22 @@
 import {Nunito_Sans } from 'next/font/google';
 
 
+const nunito_600 = Nunito_Sans({
+    weight: '600',
+    style: 'normal',
+    subsets: ['latin']
+})
+
+
 export default function Nav() {
 
     return(
-        <div>
-            Nav Section
-        </div>
+        <nav className='flex w-full py-2 px-14 justify-between items-center'>
+            <h1 className={`${nunito_600.className} text-[2.25rem] text-purple-blue
+             leading-normal`}>
+                Datei
+            </h1>
+            
+        </nav>
     )
 }

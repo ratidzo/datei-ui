0b20c06d401ce2ae2c196000c93381e5bb5715b5
ratidzo <ratygore@gmail.com>
2024-01-08 06:43:39 +0200

    dev: create directory structure for dashboard layout and pages

---
+++ app/dashboard/database/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Database() {
+    return (
+        <div>
+            <h1>
+                Student Database
+            </h1>
+        </div>
+    )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+
+    return (
+        <div>
+            <h1>
+                Dashboard Home
+            </h1>
+        </div>
+    )
+}
